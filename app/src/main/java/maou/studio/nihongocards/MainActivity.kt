f8d1b9f384704b7cc4bc816d66fce199e29a6709
vcavalca <61808793+vcavalca@users.noreply.github.com>
2024-03-20 22:01:14 -0300

    Update

--- app/src/main/java/maou/studio/nihongocards/MainActivity.kt
+++ app/src/main/java/maou/studio/nihongocards/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import maou.studio.nihongocards.databinding.ActivityMainBinding
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private  lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_NihongoCards)
@@ -18,15 +21,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // MediaPlayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.soudtrack)
+        mediaPlayer.isLooping = true
 
         // Buttons Config
         binding.musicButton.setOnClickListener {
-
+            if (mediaPlayer.isPlaying){
+                mediaPlayer.pause()
+                binding.musicButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.music_off_round, 0,0,0)
+            } else {
+                mediaPlayer.start()
+                binding.musicButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.music_note_round, 0,0,0)
+            }
         }
 
         binding.startButton.setOnClickListener {
@@ -37,5 +44,28 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer.start()
+        binding.musicButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.music_note_round, 0,0,0)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
 }
